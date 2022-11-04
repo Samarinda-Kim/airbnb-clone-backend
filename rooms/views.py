e5213be2bf35d2fac84e3272b4eb8cc6e1d88e16
Samarinda-Kim <ittsgoodman@gmail.com>
2022-11-04 23:40:11 +0900

    #11.22 Bookings

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,8 @@
 from ast import Is, Not
 from django.conf import settings
+from django.utils import timezone
 from tkinter.font import ROMAN
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
@@ -17,7 +19,8 @@ from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerial
 from rooms import serializers
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from bookings.models import Booking
+from bookings.serializers import PublicBookingSerializer
 
 
 class Amenities(APIView):
@@ -263,4 +266,24 @@ class RoomPhotos(APIView):
 
 class RoomBookings(APIView):
 
-    permission_classes = []
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        now = timezone.localtime(timezone.now()).date()
+        bookings = Booking.objects.filter(
+            room=room,
+            kind=Booking.BookingKindChoices.ROOM,
+            check_in__gt=now,
+        )
+        serializer = PublicBookingSerializer(
+            bookings,
+            many=True,
+        )
+        return Response(serializer.data)
