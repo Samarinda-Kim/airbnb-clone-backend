8ae13f9b71f97b7f71716a6d7fa3b1f800b9ef63
Samarinda-Kim <ittsgoodman@gmail.com>
2022-12-04 23:28:21 +0900

    #14.9 Permissions

--- rooms/schema.py
+++ rooms/schema.py
@@ -1,13 +1,17 @@
 import strawberry
 import typing
+from strawberry.types import Info
 from . import types
 from . import queries
+from strawberry.permission import BasePermission
+from common.permissions import OnlyLoggedIn
 
 
 @strawberry.type
 class Query:
     all_rooms: typing.List[types.RoomType] = strawberry.field(
         resolver=queries.get_all_rooms,
+        permission_classes=[OnlyLoggedIn],
     )
     room: typing.Optional[types.RoomType] = strawberry.field(
         resolver=queries.get_room,
