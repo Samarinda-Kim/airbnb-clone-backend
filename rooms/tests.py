f9a2cccf63d6c97c57f16d3f065e66d93514e39f
Samarinda-Kim <ittsgoodman@gmail.com>
2022-12-19 01:18:33 +0900

    #16.4 Amenity Detail Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -42,7 +42,7 @@ class TestAmenities(APITestCase):
     def test_create_amenity(self):
 
         new_amenity_name = "New Amenity"
-        new_amenity_description = "New Amenity Desc."
+        new_amenity_description = "New Amenity Desc"
 
         response = self.client.post(
             self.URL,
@@ -78,3 +78,86 @@ class TestAmenities(APITestCase):
             "name",
             data,
         )
+
+
+class TestAmenity(APITestCase):
+
+    NAME = "Test Amenity"
+    DESC = "Test Dsc"
+
+    URL = "/api/v1/rooms/amenities/"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+
+        response = self.client.get("/api/v1/rooms/amenities/1")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(
+            data["name"],
+            self.NAME,
+        )
+
+        self.assertEqual(
+            data["description"],
+            self.DESC,
+        )
+
+    def test_put_amenity(self):
+
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity Desc"
+        wrong_amenity_name = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+
+        response = self.client.put(
+            f"{self.URL}1",
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+        data = response.json()
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+
+        self.assertEqual(
+            data["name"],
+            new_amenity_name,
+        )
+
+        self.assertEqual(
+            data["description"],
+            new_amenity_description,
+        )
+
+        response = self.client.put(
+            f"{self.URL}1",
+            data={"name": wrong_amenity_name},
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        def test_delete_amenity(self):
+
+            response = self.client.delete("/api/v1/rooms/amenities/1")
+
+            self.assertEqual(
+                response.status_code,
+                204,
+            )
